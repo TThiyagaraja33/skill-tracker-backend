e94aa54b54018bd95cfc08814449f2e8007ba42f
TThiyagaraja33 <109670626+TThiyagaraja33@users.noreply.github.com>
2022-08-12 13:18:03 +0530

    Updated

--- skill-tracker-persist/src/test/java/com/cts/skilltracker/persist/SkillTrackerApplicationTest.java
+++ skill-tracker-persist/src/test/java/com/cts/skilltracker/persist/SkillTrackerApplicationTest.java
@@ -2,9 +2,7 @@ package com.cts.skilltracker.persist;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class SkillTrackerApplicationTest {
 	
 	@Test
