7d03ba1381ec8eee8361435e6cb411eec99b0cfc
TThiyagaraja33 <109670626+TThiyagaraja33@users.noreply.github.com>
2022-08-12 13:16:58 +0530

    updated

--- skill-tracker-consumer/src/test/java/com/cts/skilltracker/consumer/SkilltrackerconsumerApplicationTest.java
+++ skill-tracker-consumer/src/test/java/com/cts/skilltracker/consumer/SkilltrackerconsumerApplicationTest.java
@@ -2,9 +2,7 @@ package com.cts.skilltracker.consumer;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class SkilltrackerconsumerApplicationTest {
 
 	@Test
--- skill-tracker-oauth2/src/test/java/com/cts/skilltracker/oauth/OAuth2ApplicationTest.java
+++ skill-tracker-oauth2/src/test/java/com/cts/skilltracker/oauth/OAuth2ApplicationTest.java
@@ -2,9 +2,7 @@ package com.cts.skilltracker.oauth;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class OAuth2ApplicationTest {
 	
 	@Test
